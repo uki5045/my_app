bda078acd0b956ec97fcf4a91b11d9689c0986c8
hanjinwook <uki5045@gmail.com>
2022-03-27 14:29:29 +0900

    Detail, getmovie id

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Detail = () => {
 
     const params = useParams()
 
+    const [ movie, setMovie ] = useState({})
+
+    const getMovie = async () => {
+        try {
+             const { data, status } = await axios.get(`https://api.themoviedb.org/3/movie/${params.movieId}?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US`)
+            if(status === 200) {
+                setMovie(data)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getMovie()
+    }, [])
+
     return (
         <div>
-            <h1>{params.movieId}</h1>
+            <h1>{movie.title}</h1>
         </div>
     );
 };
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
-import { Container, Card, Row, Col, Button } from 'react-bootstrap'
+import { Container,  Row } from 'react-bootstrap'
 import {Poster} from "../components";
 
 const Home = () => {
